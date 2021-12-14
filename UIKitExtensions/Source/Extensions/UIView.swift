dee06a2599e48f72f190eb05b6807ea03a88318d
Asio Otus <asio.otus.verus@icloud.com>
2021-12-14 18:22:53 +0200

    Update UIView extensions

--- UIKitExtensions/Source/Extensions/UIView.swift
+++ UIKitExtensions/Source/Extensions/UIView.swift
@@ -1,14 +1,43 @@
 import UIKit
 
 public extension UIView {
-	func pinBounds (to view: UIView) {
-		translatesAutoresizingMaskIntoConstraints = false
+	extension UIView {
+		@discardableResult
+		func squared () -> Self {
+			self.widthAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+			return self
+		}
 		
-		NSLayoutConstraint.activate([
-			topAnchor.constraint(equalTo: view.topAnchor),
-			bottomAnchor.constraint(equalTo: view.bottomAnchor),
-			leadingAnchor.constraint(equalTo: view.leadingAnchor),
-			trailingAnchor.constraint(equalTo: view.trailingAnchor)
-		])
+		@discardableResult
+		func aspectRatio (_ value: Double) -> Self {
+			self.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: value).isActive = true
+			return self
+		}
+		
+		@discardableResult
+		func centered (in view: UIView) -> Self {
+			translatesAutoresizingMaskIntoConstraints = false
+			
+			NSLayoutConstraint.activate([
+				centerXAnchor.constraint(equalTo: view.centerXAnchor),
+				centerYAnchor.constraint(equalTo: view.centerYAnchor),
+			])
+			
+			return self
+		}
+		
+		@discardableResult
+		func pinBounds (to view: UIView) -> Self {
+			translatesAutoresizingMaskIntoConstraints = false
+			
+			NSLayoutConstraint.activate([
+				topAnchor.constraint(equalTo: view.topAnchor),
+				bottomAnchor.constraint(equalTo: view.bottomAnchor),
+				leadingAnchor.constraint(equalTo: view.leadingAnchor),
+				trailingAnchor.constraint(equalTo: view.trailingAnchor)
+			])
+			
+			return self
+		}
 	}
 }
