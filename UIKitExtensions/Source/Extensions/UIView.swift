e3e6afdfe74ae6ae87997aab0467aed776210fdb
Asio Otus <asio.otus.verus@icloud.com>
2021-12-24 13:57:20 +0200

    Fix

--- UIKitExtensions/Source/Extensions/UIView.swift
+++ UIKitExtensions/Source/Extensions/UIView.swift
@@ -1,43 +1,41 @@
 import UIKit
 
 public extension UIView {
-	extension UIView {
-		@discardableResult
-		func squared () -> Self {
-			self.widthAnchor.constraint(equalTo: self.heightAnchor).isActive = true
-			return self
-		}
+	@discardableResult
+	func squared () -> Self {
+		self.widthAnchor.constraint(equalTo: self.heightAnchor).isActive = true
+		return self
+	}
+	
+	@discardableResult
+	func aspectRatio (_ value: Double) -> Self {
+		self.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: value).isActive = true
+		return self
+	}
+	
+	@discardableResult
+	func centered (in view: UIView) -> Self {
+		translatesAutoresizingMaskIntoConstraints = false
 		
-		@discardableResult
-		func aspectRatio (_ value: Double) -> Self {
-			self.widthAnchor.constraint(equalTo: self.heightAnchor, multiplier: value).isActive = true
-			return self
-		}
+		NSLayoutConstraint.activate([
+			centerXAnchor.constraint(equalTo: view.centerXAnchor),
+			centerYAnchor.constraint(equalTo: view.centerYAnchor),
+		])
 		
-		@discardableResult
-		func centered (in view: UIView) -> Self {
-			translatesAutoresizingMaskIntoConstraints = false
-			
-			NSLayoutConstraint.activate([
-				centerXAnchor.constraint(equalTo: view.centerXAnchor),
-				centerYAnchor.constraint(equalTo: view.centerYAnchor),
-			])
-			
-			return self
-		}
+		return self
+	}
+	
+	@discardableResult
+	func pinBounds (to view: UIView) -> Self {
+		translatesAutoresizingMaskIntoConstraints = false
 		
-		@discardableResult
-		func pinBounds (to view: UIView) -> Self {
-			translatesAutoresizingMaskIntoConstraints = false
-			
-			NSLayoutConstraint.activate([
-				topAnchor.constraint(equalTo: view.topAnchor),
-				bottomAnchor.constraint(equalTo: view.bottomAnchor),
-				leadingAnchor.constraint(equalTo: view.leadingAnchor),
-				trailingAnchor.constraint(equalTo: view.trailingAnchor)
-			])
-			
-			return self
-		}
+		NSLayoutConstraint.activate([
+			topAnchor.constraint(equalTo: view.topAnchor),
+			bottomAnchor.constraint(equalTo: view.bottomAnchor),
+			leadingAnchor.constraint(equalTo: view.leadingAnchor),
+			trailingAnchor.constraint(equalTo: view.trailingAnchor)
+		])
+		
+		return self
 	}
 }
