817d034300670ea8f9b0e86212d624b3a34484a6
Asio Otus <asio.otus.verus@icloud.com>
2022-01-08 17:04:17 +0200

    Add UIColor extension

--- UIKitExtensions/Source/Extensions/UIColor.swift
+++ UIKitExtensions/Source/Extensions/UIColor.swift
@@ -68,3 +68,13 @@ extension UIColor {
     
     func newGrayscale (alpha: Double) -> Self { new(white: nil, alpha: alpha) }
 }
+
+extension UIColor {
+	convenience init (hex: Int, alpha: CGFloat = 1) {
+		let red = CGFloat((hex & 0xff0000) >> 16) / 255
+		let green = CGFloat((hex & 0x00ff00) >> 8) / 255
+		let blue = CGFloat(hex & 0x0000ff) / 255
+		
+		self.init(red: red, green: green, blue: blue, alpha: alpha)
+	}
+}
