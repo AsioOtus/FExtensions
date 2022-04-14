2df1d2d8ac348453b72947682e6c947364e6480b
Asio Otus <asio.otus.verus@icloud.com>
2022-04-14 15:18:19 +0300

    Update

--- UIKitExtensions/Source/Extensions/UIStoryboard.swift
+++ UIKitExtensions/Source/Extensions/UIStoryboard.swift
@@ -21,7 +21,7 @@ public extension UIStoryboard {
 public extension UIStoryboard {
 	convenience init <T: StoryboardInstantiatable> (for: T.Type) { self.init(name: T.storyboardName, bundle: nil) }
 	
-	func instantiate <T: StoryboardInstantiatable> (_: T.Type) -> T {
+	func instantiate <T: StoryboardInstantiatable> (identifiable: T.Type) -> T {
 		instantiateViewController(withIdentifier: T.identifierInStoryboard) as! T
 	}
 
